b25fc43113867aff106b825e977d6f071b26ea69
jenahlaxamana <146303049+jenahlaxamana@users.noreply.github.com>
2023-09-28 11:00:00 +0800

    Add files via upload

---
+++ practice 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Practice 8
+	//Gwapo
+	string neym;
+	
+	cout <<"Enter your name:";
+	getline(cin,neym); //para mag bueo it input
+	cout<<"The length of your name is: "<<neym.size();
+	
+	return 0;
+}
