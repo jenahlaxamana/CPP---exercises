adcb44d9e954e561f11a46e43fb4ea189cdf9975
jenahlaxamana <146303049+jenahlaxamana@users.noreply.github.com>
2023-10-10 09:28:30 +0800

    Add files via upload

---
+++ practice 13.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+int grade;	
+	
+cout<<"Enter your grade:";	
+cin>>grade;
+	
+if(grade >= 100){
+	
+	cout<<"Class S";
+}else if(grade >= 90){
+	
+	cout<<"Class A";
+}	else if(grade >= 80){
+
+    cout<<"Class B";
+}else if(grade >= 70){
+	
+	cout<<"Class C";
+}else if(grade >= 60){
+	
+	cout<<"Class D";
+}else{
+	
+	cout<<"Wa kat rank!";
+}
+	
+	
+	
+	
+	
+	
+}
